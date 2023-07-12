809eaae121fcf44f9d69b4724f8edf07f3d44d11
der56 <byaxis.11@gmail.com>
2023-07-12 20:10:33 +0200

    last version

---
+++ TODO.md
@@ -0,0 +1,1 @@
+hacer fetch uploadPassword
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    // usar react para solo renderizado condicional o elementos que necesiten actualización en el dom con poca carga
+    react()
+  ]
+});
